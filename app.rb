36f2de07332ea9b253c16a861703c6f7ed3e111d
Daniel Lopez <tauron32@gmail.com>
2013-02-17 08:25:33 +0100

    Basic sqlite3 database with Products table

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require 'sinatra'
+require 'data_mapper'
+
+DataMapper::setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/store.db")
+
+class Product
+  include DataMapper::Resource
+  property :id, Serial
+  property :name, Text, :required => true
+  property :created_at, DateTime
+end
+DataMapper.finalize.auto_upgrade!
 
 get '/' do
   'Hello world!'
