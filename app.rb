959f38ba62b7ea6f64c64dc658d35869d5d99d9e
Daniel Lopez <tauron32@gmail.com>
2013-02-17 09:36:22 +0100

    Retrieve all Products or one product with JSON

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
-require 'data_mapper'
+require 'dm-core'
+require 'dm-migrations'
+require 'json'
 
 DataMapper::setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/store.db")
 
@@ -8,8 +10,41 @@ class Product
   property :id, Serial
   property :name, Text, :required => true
   property :created_at, DateTime
+  
+  def url
+      "/product/#{self.id}"
+  end
+  
+  def to_json(*a)
+    { 
+      'id' => self.id, 
+      'name' => self.name,
+      'created_at'=> self.created_at
+    }.to_json(*a)
+  end
+  
+  # ensure json is safe.  If invalid json is received returns nil
+  def self.parse_json(body)
+    json = JSON.parse(body)
+    ret = { :name => json['name'], :created_at => json['created_at'] }
+    return nil if REQUIRED.find { |r| ret[r].nil? }
+
+    ret 
+  end
+  
 end
-DataMapper.finalize.auto_upgrade!
+DataMapper.auto_upgrade!  
+
+# return as JSON
+get '/products' do
+  content_type 'application/json'
+  { 'content' => Array(Product.all) }.to_json
+end
+
+get '/product/:id' do
+  content_type 'application/json'
+  { 'content' => Product.get(params[:id]) }.to_json
+end
 
 get '/' do
   @products = Product.all :order => :id.asc
@@ -23,8 +58,4 @@ get '/new/:name' do
   n.created_at = Time.now
   n.save
   redirect '/'
-end
-
-get '/product/:id' do
-  Product.get params[:id]
 end
